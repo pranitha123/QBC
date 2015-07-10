35016904f66565fd0bbb41f8ec9977e9e231caee
madhu <madhu@mytrits.com>
2015-07-10 14:29:22 +0530

    Pandari commiting Demo3 class

--- Gidemoproject/src/com/git/beans/Demo3.java
+++ Gidemoproject/src/com/git/beans/Demo3.java
@@ -1,5 +1,10 @@
 package com.git.beans;
 
 public class Demo3 {
-
+/////Demo3 
+	
+	
+	
+	
+	//Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.
 }
